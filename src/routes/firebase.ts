c0b967178fa1011637ba2c872af39afe35d5e9b0
김혜인 <dana019@naver.com>
2024-03-18 22:07:55 +0900

    Feat: Add Authentication

---
+++ src/routes/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMj3V75Tpus4EKseGm4l_E5Br8SvSK6Kg",
+  authDomain: "nwitter-3c8eb.firebaseapp.com",
+  projectId: "nwitter-3c8eb",
+  storageBucket: "nwitter-3c8eb.appspot.com",
+  messagingSenderId: "6036381740",
+  appId: "1:6036381740:web:fcd424cfeab49addb0b189",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
